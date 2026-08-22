023b5e94e3cde238ccae9db49884f0ad3e6b14ae
rafacastillo2006 <raficastillo2006@gmail.com>
2026-08-22 09:47:31 -0600

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,9 @@
 package org.example;
 
-<<<<<<< HEAD
 
 public class Main {
     static void main() {
 
         new PantallaInicioSesion(new GestionEmpleados());
     }
-=======
-public class Main {
-    static void main() {
-       
-        }
-    
->>>>>>> 88f1ee70dc91f12351560699dac0b34828013ba9
-}
+
