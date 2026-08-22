2d134a95fb3f9782a3c5bd7f21f74413740b17ad
bell0-0-dot <bella_serrano@unitec.edu>
2026-08-22 09:24:51 -0600

    Clases d biblioteca, enum

---
+++ src/main/java/org/example/Libros.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.awt.Image;
+
+
+public abstract class Libros extends MaterialBibliografico{
+    private String autor;
+    private int numeroPaginas;
+    private int ISBN;
+
+    public Libros(String autor, int numeroPaginas, int ISBN, String titulo, int codigo, int diasMaximo, NivelComplejidad nivel, Image caratula) {
+        super(titulo, codigo, diasMaximo, nivel, caratula);
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+        this.ISBN = ISBN;
+    }
+
+    @Override
+    public String obtenerDescripcion(){
+         return "Libro: \"" + titulo + "\" de " + autor +
+           " (" + numeroPaginas + " páginas, ISBN: " + ISBN + ")";
+    }
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/org/example/Revistas.java
@@ -0,0 +1,46 @@
+
+package org.example;
+import java.awt.Image;
+
+public abstract class Revistas extends MaterialBibliografico{
+    private int numeroEdicion;
+    private Periocidad periocidad;
+
+    public Revistas(int numeroEdicion, Periocidad periodicidad, String titulo, int codigo,
+                   int diasMaximo, NivelComplejidad nivel, Image caratula) {
+        super(titulo, codigo, diasMaximo, nivel, caratula);
+        this.numeroEdicion = numeroEdicion;
+        this.periocidad = periocidad;
+    }
+    
+    @Override
+    public String obtenerDescripcion(){
+        return "Revista: " + titulo + " (Edición #" 
+                + numeroEdicion + ", " + periocidad + ")";
+    }
+    @Override
+    public int calcularDias(){
+        int diasBase=diasMaximo;
+        return diasBase+nivel.getDiasAdicionales();
+    
+    }
+
+    public int getNumeroEdicion() {
+        return numeroEdicion;
+    }
+
+    public Periocidad getPeriocidad() {
+        return periocidad;
+    }
+
+    public void setNumeroEdicion(int numeroEdicion) {
+        this.numeroEdicion = numeroEdicion;
+    }
+
+    public void setPeriocidad(Periocidad periocidad) {
+        this.periocidad = periocidad;
+    }
+    
+    
+    
+}
